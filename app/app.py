8c7b82dda256bfe18a1a36be192d3b01f6bc7ebc
Mohammad abbas <mohammed3bbas99@gmail.com>
2023-09-16 00:47:32 +0300

    Handle expected errors

--- app/app.py
+++ app/app.py
@@ -5,7 +5,37 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+# Initialize an empty list to store message history
 messages_history = []
+
+# System prompt to set initial behavior
+SYSTEM_PROMPT = {
+    "role": "system",
+    "content": "You are a friendly and adaptable conversational partner. Strive to provide engaging and helpful responses while maintaining a warm and approachable tone throughout the conversation."
+}
+
+
+def generate_completion(user_message):
+    """Generate a completion using the user's message"""
+    try:
+        if (not messages_history):
+            messages_history.append(SYSTEM_PROMPT)
+
+        # Generate a completion using OpenAI APIs
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo-0613",
+            messages=messages,
+        )
+
+        # Assistant's message response
+        completion_message = completion.choices[0].message
+
+        # Add the assistant's response to the message history
+        messages_history.append(completion_message)
+
+        return completion_message
+    except openai.error.OpenAIError as e:
+        raise OpenAIError(str(e))
 
 
 @app.route("/", methods=["GET"])
@@ -16,19 +46,14 @@ def welcome():
 @app.route("/chat", methods=["POST"])
 def chat():
     user_message = request.json.get("message")
-    if (not messages_history):
-        messages_history.append({
-            "role": "system",
-            "content": "You are a friendly and adaptable conversational partner. Strive to provide engaging and helpful responses while maintaining a warm and approachable tone throughout the conversation."
-        })
-    messages_history.append({"role": "user", "content": user_message})
 
-    completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo-0613",
-        messages=messages_history,
-    )
+    # Handle missing user message
+    if not user_message:
+        return jsonify({"error": "Missing 'message' in request JSON"}), 400
 
-    completion_message = completion.choices[0].message
-    messages_history.append(completion_message)
+    try:
+        completion_message = generate_completion(user_message)
 
-    return jsonify(completion_message)
+        return jsonify(completion_message)
+    except OpenAIError as e:
+        return jsonify({"error": str(e)}), 500
