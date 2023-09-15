3fd8116cb97158249ff8197ada8cfed3ba457193
Mohammad abbas <mohammed3bbas99@gmail.com>
2023-09-16 00:14:41 +0300

    Add system role prompt

--- app/app.py
+++ app/app.py
@@ -7,13 +7,20 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 messages_history = []
 
+
 @app.route("/", methods=["GET"])
 def welcome():
     return "Welcome to ChatGPT API Wrapper!"
 
+
 @app.route("/chat", methods=["POST"])
 def chat():
     user_message = request.json.get("message")
+    if (not messages_history):
+        messages_history.append({
+            "role": "system",
+            "content": "You are a friendly and adaptable conversational partner. Strive to provide engaging and helpful responses while maintaining a warm and approachable tone throughout the conversation."
+        })
     messages_history.append({"role": "user", "content": user_message})
 
     completion = openai.ChatCompletion.create(
@@ -22,6 +29,6 @@ def chat():
     )
 
     completion_message = completion.choices[0].message
-    messages_history.append({"role": "assistant", "content": completion_message})
+    messages_history.append(completion_message)
 
     return jsonify(completion_message)
